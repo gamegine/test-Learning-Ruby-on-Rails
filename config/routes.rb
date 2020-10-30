2d09e90458bee5ab80e5112d5ed1e0910b4c234a
gamegine <gamegine@protonmail.com>
2020-10-30 11:29:33 +0100

    game link to review

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :games
+  resources :games do
+    resources :review, only: [ :new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'test', to: 'pages#test'
   get 'link', to: 'pages#link'
